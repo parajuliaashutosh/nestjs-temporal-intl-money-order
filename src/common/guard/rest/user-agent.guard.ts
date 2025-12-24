10b7bda9b3616b74e30e260268e407f9c3c89f7c
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-12-24 16:31:57 +0545

    feat: add User decorator, AuthenticationGuard, and UserAgentGuard for enhanced request handling and user authentication

--- src/common/guard/rest/user-agent.guard.ts
+++ src/common/guard/rest/user-agent.guard.ts
@@ -1,8 +1,8 @@
 import {
-    CanActivate,
-    ExecutionContext,
-    ForbiddenException,
-    Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
