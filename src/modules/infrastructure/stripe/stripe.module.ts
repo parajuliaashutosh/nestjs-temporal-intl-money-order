d4db8eb8e217bc4554cc7120234ef4b4a71d2c69
Aashutosh Parajuli <aashutosh282@gmail.com>
2026-02-10 17:10:24 +0545

    feat: add AuthModule to StripeModule imports and clean up SystemConfigModule imports

--- src/modules/infrastructure/stripe/stripe.module.ts
+++ src/modules/infrastructure/stripe/stripe.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Stripe } from 'stripe';
+import { AuthModule } from '../../auth/auth.module';
 import { WalletModule } from '../../wallet/wallet.module';
 import { StripeController } from './gateway/rest/stripe.controller';
 import { StripeService } from './service/stripe.service';
 import { STRIPE, STRIPE_SERVICE } from './stripe.constant';
 
 @Module({
-  imports: [WalletModule],
+  imports: [WalletModule, AuthModule],
   providers: [
     {
       provide: STRIPE,
