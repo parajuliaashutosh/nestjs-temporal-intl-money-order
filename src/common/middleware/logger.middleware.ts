fd865d2e558ea4d2e5edd29484989e60a53a70a5
Aashutosh Parajuli <aashutosh282@gmail.com>
2026-06-10 15:15:02 +0545

    update logger

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
@@ -7,14 +8,21 @@ export class LoggerMiddleware implements NestMiddleware {
 
   use(req: Request, res: Response, next: NextFunction) {
     const { method, originalUrl, ip } = req;
+    const requestId = uuidv4();
     const start = Date.now();
 
-    this.logger.log(`${method} ${originalUrl} [${ip}]`);
+    res.setHeader('X-Request-Id', requestId);
+
+    this.logger.log(
+      `[HTTP REQ] [${requestId}] ${method} ${originalUrl} [${ip}]`,
+    );
 
     res.on('finish', () => {
       const ms = Date.now() - start;
       const { statusCode } = res;
-      this.logger.log(`${method} ${originalUrl} ${statusCode} ${ms}ms`);
+      this.logger.log(
+        `[HTTP RES] [${requestId}] ${method} ${originalUrl} ${statusCode} ${ms}ms`,
+      );
     });
 
     next();
